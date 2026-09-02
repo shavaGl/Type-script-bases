da807b076e53cc86530ad22ebcf0968028f6af13
Salvador Guerrero Lorenzo <ing.salvador.gl@gmail.com>
2026-09-01 22:50:00 -0600

    Se corrrije el nombre del ejercicio

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 //import './bases/01-ejecucionyTiposPrimitivos/01-ejecucionyTiposPrimitivos-Problema1.ts';
 //import './bases/01-ejecucionyTiposPrimitivos/01-ejecucionyTiposPrimitivos-Problema2.ts'
-import './bases/02-OperadoresyCoerción/02-OperadoresyCoerción.ts'
+import './bases/02-OperadoresyCoerción/02-OperadoresyCoerción-Problema1.ts'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
